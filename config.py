23ac4d806e33beeb774436f12ebe58dea155ab09
icerain0824 <139022390@qq.com>
2016-12-04 22:24:46 +0800

    a

--- config.py
+++ config.py
@@ -14,5 +14,5 @@ class DevConfig(Config):
     """Development config class."""
     #Open the DEBUG
     DEBUG = True
-    HOST = '0.0.0.0'
-
+    #Mysql connection
+    SQLALCHEMY_DATABASE_URI =  'mysql+pymysql://root:123456@127.0.0.1:3306/myblog?charset=utf8'
--- manage.py
+++ manage.py
@@ -5,7 +5,8 @@
 from flask_script import Manager ,Server
 
 import main
-
+import models
+import tab
 #Init manager object via app object
 
 manager = Manager(main.app)
@@ -24,7 +25,9 @@ def make_shell_context():
     type: `Dict`
     """
     # 确保有导入 Flask app object，否则启动的 CLI 上下文中仍然没有 app 对象
-    return dict(app=main.app)
+    return dict(app=main.app,
+                db=models.db,
+                User=models.User)
 
 if __name__=='__main__':
     manager.run()
