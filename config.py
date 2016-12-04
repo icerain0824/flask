6131776e4dc25543db2564fe1e6149daf29ba490
icerain0824 <139022390@qq.com>
2016-12-04 19:03:22 +0800

    第一天

---
+++ config.py
@@ -0,0 +1,18 @@
+
+
+class Config(object):
+    """Base config class."""
+
+    pass
+
+class ProdConfig(Config):
+    """Production config class."""
+    pass
+
+
+class DevConfig(Config):
+    """Development config class."""
+    #Open the DEBUG
+    DEBUG = True
+    HOST = '0.0.0.0'
+
